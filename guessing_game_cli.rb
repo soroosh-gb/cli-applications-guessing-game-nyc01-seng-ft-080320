b734c43408d3075b6abd9f7db6eb995444401447
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-20 18:09:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def run_guessing_game
   # result = ""
-  user_input = gets.chomp 
-  random_number = rand(6) + 1 
+  user_input = gets.chomp
+  random_number = (rand(6) + 1).to_s 
    
   if user_input == random_number
     puts "You guessed the correct number!"
