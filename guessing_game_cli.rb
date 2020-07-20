25c6e99b3d93311b7c51c3416036649ad5426402
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-20 18:09:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -18,33 +18,6 @@ def run_guessing_game
   # binding.pry
 end 
  
-# def get_user_input()
-#   user_input = gets.chomp.to_i
-# end
- 
-# def random_num()
-#   random_number = rand(6) + 1 
-# end 
- 
-# def output(user_input)
-#   user_input = get_user_input()
-#   random_number = random_number()
-#   result = ""
-#   if user_input == random_number
-#     result = "You guessed the correct number!"
-#   elsif user_input != random_number && user_input != "exit"
-#     result = "Sorry! The computer guessed #{random_number}."
-#   elsif user_input == "exit"
-#     result = "Goodbye!"
-#   else 
-#     result = "Invalid input!"
-#   end 
-#   puts result
-# end
-
-# def run_guessing_game
-#   output()
-# end
 
    
 
